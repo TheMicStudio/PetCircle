c24d3c87143573ae990ffc6150d74eddb41fb298
Mateis Bourlet <mateisbourlet9@gmail.com>
2026-09-09 18:35:41 +0200

    fix 2 import feedpage

--- frontend/src/features/posts/usePosts.ts
+++ frontend/src/features/posts/usePosts.ts
@@ -1,7 +1,7 @@
 import type { FeedPage, FeedPost } from '@petcircle/contracts';
 import { useApiMutation } from '../../shared/api/mutation/useMutation';
 import { useApiQuery } from '../../shared/api/query/useQuery';
-import type { FeedPage, FeedPost } from "@petcircle/contracts";
+
 
 export const useCreatePost = () => {
     return useApiMutation<FeedPost, FormData>('POST', '/posts');
